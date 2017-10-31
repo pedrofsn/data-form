d97bb09011d8720aee8ca5cf19528dcb5091f38b
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2017-10-31 12:25:36 -0200

    Initial commit

---
+++ app/src/main/java/br/redcode/dataform/ActivityMain.kt
@@ -0,0 +1,12 @@
+package br.redcode.dataform
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ActivityMain : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
