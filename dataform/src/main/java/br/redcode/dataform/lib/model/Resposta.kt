c3beb1716187f97bc1fc56cb51b1301a4c2cc651
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2017-11-03 23:18:45 -0200

    Método para averiguar se o objeto resposta de fato contém uma resposta válida

--- dataform/src/main/java/br/redcode/dataform/lib/model/Resposta.kt
+++ dataform/src/main/java/br/redcode/dataform/lib/model/Resposta.kt
@@ -6,9 +6,21 @@ import java.io.Serializable
  * Created by pedrofsn on 31/10/2017.
  */
 data class Resposta(
+        var idPergunta: Int? = null,
         var resposta: String? = null,
 
         var alternativa: Alternativa? = null,
         var alternativas: List<Alternativa>? = null,
         var imagens: List<Imagem>? = null
-) : Serializable
+) : Serializable {
+
+    fun hasResposta(): Boolean {
+        return idPergunta != null
+                || resposta != null && resposta?.isNotEmpty() ?: false
+                || alternativa != null
+                || alternativas != null && alternativas?.isNotEmpty() ?: false
+                || imagens != null && imagens?.isNotEmpty() ?: false
+    }
+
+}
+
