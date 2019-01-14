9ba4f3bf2a38fae45d74b5b7adeb0c254ac70bfa
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-01-14 11:53:38 -0200

    supressing DEPRECATION with TODO

--- dataform/src/main/java/br/redcode/dataform/lib/ui/UIPerguntaTextual.kt
+++ dataform/src/main/java/br/redcode/dataform/lib/ui/UIPerguntaTextual.kt
@@ -9,7 +9,7 @@ import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import br.redcode.dataform.lib.R
 import br.redcode.dataform.lib.domain.UIPerguntaGeneric
-import br.redcode.dataform.lib.interfaces.Perguntavel
+import br.redcode.dataform.lib.interfaces.Questionable
 import br.redcode.dataform.lib.model.ConfiguracaoFormulario
 import br.redcode.dataform.lib.model.Pergunta
 import br.redcode.dataform.lib.model.Resposta
@@ -17,7 +17,7 @@ import br.redcode.dataform.lib.model.Resposta
 /**
  * Created by pedrofsn on 31/10/2017.
  */
-class UIPerguntaTextual(private val contextActivity: Context, pergunta: Pergunta, configuracao: ConfiguracaoFormulario) : UIPerguntaGeneric(contextActivity, R.layout.ui_pergunta_textual, pergunta, configuracao), Perguntavel {
+class UIPerguntaTextual(private val contextActivity: Context, pergunta: Pergunta, configuracao: ConfiguracaoFormulario) : UIPerguntaGeneric(contextActivity, R.layout.ui_pergunta_textual, pergunta, configuracao), Questionable {
 
     private lateinit var editText: EditText
 
@@ -32,6 +32,8 @@ class UIPerguntaTextual(private val contextActivity: Context, pergunta: Pergunta
         pergunta.resposta?.resposta?.let { editText.setText(it) }
         editText.isEnabled = configuracao.editavel
         if (configuracao.editavel.not()) {
+            // TODO try to migrete from this code bellow to TextViewCompat.setTextAppearance(editText, android.R.style.TextAppearance_Widget_TextView) (FIX: https://stackoverflow.com/a/37028325/1565769)
+            @Suppress("DEPRECATION")
             editText.setTextAppearance(contextActivity, android.R.style.TextAppearance_Widget_TextView)
             editText.setTypeface(null, Typeface.ITALIC)
         }
@@ -54,14 +56,14 @@ class UIPerguntaTextual(private val contextActivity: Context, pergunta: Pergunta
         }
     }
 
-    override fun getResposta(): Resposta {
+    override fun getAnswer(): Resposta {
         val resposta = Resposta(resposta = editText.text.toString().trim())
         if (pergunta.resposta != null) resposta.tag = pergunta.resposta?.tag
         pergunta.resposta = resposta
         return resposta
     }
 
-    override fun isPreenchidoCorretamente() = editText.text.toString().trim().isNotEmpty()
-    override fun getMensagemErroPreenchimento() = contextActivity.getString(R.string.o_campo_de_texto_nao_foi_preenchido)
+    override fun isFilledCorrect() = editText.text.toString().trim().isNotEmpty()
+    override fun getMessageErrorFill() = contextActivity.getString(R.string.o_campo_de_texto_nao_foi_preenchido)
 
 }
