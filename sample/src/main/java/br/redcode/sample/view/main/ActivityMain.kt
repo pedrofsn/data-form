e4c0670b5dc2daaf4f4cf1454f216830df5ee58b
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-02-25 20:48:05 -0300

    - Improving UI sample home

--- sample/src/main/java/br/redcode/sample/view/main/ActivityMain.kt
+++ sample/src/main/java/br/redcode/sample/view/main/ActivityMain.kt
@@ -2,9 +2,11 @@ package br.redcode.sample.view.main
 
 import android.view.View
 import br.redcode.sample.R
+import br.redcode.sample.data.database.Mock
 import br.redcode.sample.domain.ActivityWithoutMVVM
 import br.redcode.sample.view.questions.QuestionsActivity
 import br.redcode.sample.view.questions.QuestionsActivity.Companion.LOAD_FORM_FROM_JSON
+import kotlinx.coroutines.launch
 
 /*
     CREATED BY @PEDROFSN
@@ -18,5 +20,9 @@ class ActivityMain(override val layout: Int = R.layout.activity_main) : Activity
 
     fun openCase1(view: View?) = goTo<QuestionsActivity>("case" to LOAD_FORM_FROM_JSON)
     fun openCase2(view: View?) = goTo<QuestionsActivity>("case" to QuestionsActivity.LOAD_FORM_FROM_DATABASE)
+
+    fun resetDatabase(view: View?) {
+        launch(io()) { Mock.seedDatabase() }
+    }
 
 }
